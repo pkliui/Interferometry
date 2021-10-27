37173955876a7cfcd0aadd9b0ed43b214dbcb67d
pkliui <kliuiev@pm.me>
2021-10-27 18:29:26 +0300

    added normalize method and add checking the number of samples in wigner distr

--- Interferometry/classes/base.py
+++ Interferometry/classes/base.py
@@ -15,7 +15,7 @@ class BaseInterferometry:
     def __init__(self):
         super().__init__()
 
-    def compute_spectrogram(self, signal, delta_f, nperseg=2**6, plotting=False, **kwargs):
+    def compute_spectrogram(self, signal, time_step, nperseg=2**6, plotting=False, **kwargs):
         """
         Compute a spectrogram of a time-series signal by short time Fourier transform (STFT)
         ---
@@ -23,8 +23,9 @@ class BaseInterferometry:
         ---
         signal: a 1D numpy array of floats
             time series to compute the spectrogram of
-        delta_f: float
-            Inverse of the sampling interval in time domain  (1 / self.delta_t), 1 / s
+            normalized so that the interferogram's base level is 1
+        time_step: float
+            Sampling interval in time domain, s
         nperseg: int, optional
             window size of the STFT
         plotting: bool, opional
@@ -42,7 +43,7 @@ class BaseInterferometry:
         """
         #
         # first looking at the power of the short time fourier transform (SFTF):
-        f_stft_samples, t_stft_samples, signal_stft = stft(signal, delta_f, nperseg=nperseg,
+        f_stft_samples, t_stft_samples, signal_stft = stft(signal, 1/time_step, nperseg=nperseg,
                                                            noverlap=nperseg-1, return_onesided=False, **kwargs)
         #
         # shift the frequency axis
@@ -52,26 +53,23 @@ class BaseInterferometry:
         # generate frequency and time steps - must be replaced by  base class variables!
         df1 = f_stft_samples[1] - f_stft_samples[0]
         dt = t_stft_samples[1] - t_stft_samples[0]
-        #print("t_stft_samples[0] - dt / 2 ", t_stft_samples[0] - dt / 2)
-        #print("t_stft_samples[-1] + dt / 2", t_stft_samples[-1] + dt / 2)
-        #print("f_stft_samples[0] - df1 / 2 ", f_stft_samples[0] - df1 / 2)
-        #print("f_stft_samples[-1] + df1 / 2", f_stft_samples[-1] + df1 / 2)
-        #
-        #print(signal_stft.shape)
         #
         if plotting:
             f, axx = plt.subplots(1)
-            im = axx.imshow(np.abs(signal_stft), aspect='auto',
-                               interpolation=None, origin='lower',
-                               extent=(t_stft_samples[0] - dt / 2, t_stft_samples[-1] + dt / 2,
-                                       f_stft_samples[0] - df1 / 2, f_stft_samples[-1] + df1 / 2))
+            im = axx.imshow(np.abs(signal_stft),
+                               interpolation=None, origin='lower', aspect="auto",
+                               extent=(
+                                       t_stft_samples[0] - dt / 2, t_stft_samples[-1] + dt / 2,
+                                       f_stft_samples[0] - df1 / 2, f_stft_samples[-1] + df1 / 2),
+                            cmap="gnuplot2"
+                            )
             axx.set_ylabel('frequency [Hz]')
             plt.colorbar(im, ax=axx)
             axx.set_title('spectrogram - amplitude of STFT')
         #
         return signal_stft, t_stft_samples, f_stft_samples
 
-    def compute_wigner_ville_distribution(self, time_samples, signal, plotting=False):
+    def compute_wigner_ville_distribution(self, time_samples, signal, plotting=False, vmin=0, vmax=0):
         """
         Compute Wigner-Ville distribution (time-frequency representation) of a time-series signal
         ---
@@ -92,43 +90,71 @@ class BaseInterferometry:
             Array of frequency samples
         """
         #
-        # compute WVD
-        wvd = tftb.processing.WignerVilleDistribution(signal, timestamps=time_samples)
-        #print("signal ", signal.shape)
-        #print("time sample", time_samples.shape)
+        if len(time_samples) == len(signal):
+            # check if the number of samples is even, if not - delete the last entry
+            if (len(time_samples) % 2 != 0) and (len(signal) % 2 != 0):
+                time_samples = time_samples[:-1]
+                signal = signal[:-1]
+            else:
+                pass
+            #
+            # compute WVD
+            wvd = tftb.processing.WignerVilleDistribution(signal, timestamps=time_samples)
+            #
+            # t_wvd_samples is equivalent to time_samples
+            # f_wvd_samples are the "normalized frequencies" which I override below
+            signal_wvd, t_wvd_samples, f_wvd_samples = wvd.run()
+            # #
+            # # generate time steps - must be replaced by base class variables!
+            delta_t = time_samples[1] - time_samples[0]
+            # #
+            # # because of the way WignerVilleDistribution is implemented,
+            # # the maximum frequency is half of the sampling Nyquist frequency,
+            # # (e.g. 1 Hz instead of 2 Hz set by , and the sampling is 2 * dt instead of dt
+            # # hence generate new freq. samples
+            f_wvd_samples = np.fft.fftshift(np.fft.fftfreq(signal_wvd.shape[0], d=2*delta_t))
+            # #
+            # # generate frequency steps - must be replaced by  base class variables!
+            delta_f = f_wvd_samples[1] - f_wvd_samples[0]
+            # #
+            if plotting:
+                 f, axx = plt.subplots(1)
+                 im = axx.imshow(np.fft.fftshift((signal_wvd)**1.0, axes=0),
+                                 aspect='auto', origin='lower',
+                                 extent=(time_samples[0] - delta_t / 2, time_samples[-1] + delta_t / 2,
+                                            f_wvd_samples[0] - delta_f / 2, f_wvd_samples[-1] + delta_f / 2),
+                                 cmap = plt.get_cmap("hsv"), vmin=vmin, vmax=vmax)
+                 axx.set_ylabel('frequency [Hz]')
+                 plt.colorbar(im, ax=axx)
+                 axx.set_title("amplitude of Wigner-Ville distr.")
+        else:
+            raise ValueError("Time_samples and signal must have the same length!")
+
+    def normalize(self, signal, time_step, normalizing_width=10e-15):
+        """
+        Normalizes an interferogram to have zero mean and maximum intensity of 1
+        ---
+        Parameters
+        ---
+        signal: ndarray
+            timer series to normalize
+        time_step:
+            temporal step the signal was recorded at
+        normalizing_width: float, optional
+            the width of integration range to be used for signals' mormalization, in seconds
+        ---
+        Return
+        ---
+        signal_norm: ndarray
+            normalized signal
+        """
+        # set integration range for normalization
+        int_range = int(normalizing_width / time_step)
         #
-        # t_wvd_samples is equivalent to time_samples
-        # f_wvd_samples are the "normalized frequencies" which I override below
-
-        #wvd.plot(threshold=1.9, show_tf=True)
-        #wvd.run()
-        signal_wvd, t_wvd_samples, f_wvd_samples = wvd.run()
-        print("sign  ", signal_wvd.shape)
-        print("t ", t_wvd_samples.shape)
-        print("f  ", f_wvd_samples.shape)
-
-        # #
-        # # generate time steps - must be replaced by  base class variables!
-        # delta_t = time_samples[1] - time_samples[0]
-        # #
-        # # because of the way WignerVilleDistribution is implemented,
-        # # the maximum frequency is half of the sampling Nyquist frequency,
-        # # (e.g. 1 Hz instead of 2 Hz set by , and the sampling is 2 * dt instead of dt
-        # # hence generate new freq. samples
-        # f_wvd_samples = np.fft.fftshift(np.fft.fftfreq(signal_wvd.shape[0], d=2*delta_t))
-        # #
-        # # generate frequency steps - must be replaced by  base class variables!
-        # df1 = f_wvd_samples[1] - f_wvd_samples[0]
-        # #
-        # if plotting:
-        #     f, axx = plt.subplots(1)
-        #     im = axx.imshow(np.abs(signal_wvd), aspect='auto',
-        #                        interpolation=None, origin='lower',
-        #                        extent=(time_samples[0] - delta_t / 2, time_samples[-1] + delta_t / 2,
-        #                                f_wvd_samples[0] - df1 / 2, f_wvd_samples[-1] + df1 / 2))
-        #     axx.set_ylabel('frequency [Hz]')
-        #     plt.colorbar(im, ax=axx)
-        #     axx.set_title("amplitude of Wigner-Ville distr.")
-
-        return signal_wvd, t_wvd_samples, f_wvd_samples
-
+        # compute the mean value of the signal's background for given integration range
+        # and normalize signal to have zero mean and max intensity of 1
+        signal_mean_bg = np.mean(np.abs(np.array(signal[0:int_range])))
+        signal = signal - signal_mean_bg
+        signal_norm = signal / signal.max()
+        #
+        return signal_norm
