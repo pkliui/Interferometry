2f960ad0ebd8e84841a46ae57d9d4eb2dbfa9541
pkliui <kliuiev@pm.me>
2022-01-31 18:25:26 +0200

    added docstring to interferogram_objective_function

--- Interferometry/modules/minimization.py
+++ Interferometry/modules/minimization.py
@@ -5,22 +5,31 @@ import numpy as np
 from scipy.optimize import minimize
 
 
-def interferogram_objective_function(gen_complex_interferogram, measured_signal, field_ac_weight, interferometric_ac_weight):
+def interferogram_objective_function(simulated_signal, measured_signal):
     """
     Defines the difference between simulated and measured interferograms (by least-squares regression)
+    ---
+    Args:
+    ---
+    simulated_signal: 1d array of floats
+        simulated interferogram samples
+    measured_signal: 1d array of floats
+        measured interferogram samples
+    ---
+    Returns:
+    ---
+    obj_fun: float
+        Value of the objective function to be minimized
     """
-    simulated_signal = gen_complex_interferogram(field_ac_weight=field_ac_weight, interferometric_ac_weight=interferometric_ac_weight,
-                                                 temp_shift=0, plotting=False)
     obj_fun = np.sum((simulated_signal - measured_signal)**2)
 
     return obj_fun
+
 
 def find_best_mixture_of_interferograms(obj_fun, gen_complex_interferogram, measured_signal):
     # define the bounds for the cutoff frequency
     bounds = [(0,15)]
     res = minimize(lambda coeffs: obj_fun(gen_complex_interferogram, measured_signal, *coeffs), x0=np.array([10]), bounds=bounds,
                    method='SLSQP', tol=1e-10, options={'disp': True})
-    #plt.plot(170e12*fc, self.objective_function(170e12*fc, filter_order=4), "+")
-    #plt.show()
 
     return res
