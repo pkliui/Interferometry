60e9c70189e4a5fa8797ca6028634f4c3b0d03d4
pkliui <kliuiev@pm.me>
2022-03-14 01:56:15 +0200

    added IFT

--- Interferometry/modules/fourier_transforms.py
+++ Interferometry/modules/fourier_transforms.py
@@ -36,3 +36,24 @@ def ft_data(intensity, time_step, time_samples):
     ft = np.fft.rfft(intensity)[1:]
     freq = np.fft.rfftfreq(len(time_samples), time_step)[1:]
     return ft, freq
+
+def ift_data(ft_data):
+    """
+    Computes the inverse Fourier transform of an input sequence
+    and the corresponding frequency samples, given the signal_data intensity samples, temporal samples and a discretization step
+    ---
+    Parameters
+    ---
+    ft_data: numpy 1D array
+        Signal FT distribution samples
+    ---
+    Return
+    ---
+    ift: 1d numpy array
+        Only positive frequencies of the inverse Fourier transformed sequence
+        Excludes the zeroth frequency
+    """
+    #
+    # begin from 1st element to avoid displaying the zero-th freq. component
+    ift = np.fft.irfft(ft_data)[1:]
+    return ift
